1d1179cc706edfeec47ddd36b4949a5c642498e5
Hering, Martin (extern) <martin.hering.extern@prosiebensat1.com>
2020-09-23 23:10:10 +0200

    use docs

--- pages/index.vue
+++ pages/index.vue
@@ -7,7 +7,6 @@
       scrolling="no"
       frameborder="no"
       allow="autoplay"
-      style="padding: 0px 42px"
       src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1037349952&color=%23000000&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
     ></iframe>
     <div
