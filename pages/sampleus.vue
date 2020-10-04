a54fbd0c512bd57a82259e5661ed624000a4196a
Hering, Martin (extern) <martin.hering.extern@prosiebensat1.com>
2020-10-04 19:09:59 +0200

    add sample png

--- pages/sampleus.vue
+++ pages/sampleus.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="container my-8" style="background-color: #f4e9e2">
+    <img src="/kugel.jpg" alt="Andromedar" class="image" />
     <div class="my-3">
       <h2 class="text-2xl my-4">Samples</h2>
-      <p class="my-2">You can download a pack with samples from Andromedar.</p>
+      <p class="my-2">You can download a pack with ANDROMEDAR samples.</p>
 
       <a
         href="https://www.dropbox.com/sh/69rz3ygywc4bu04/AACx1LO3G-ZK26-0CmRI-Wwya?dl=0"
